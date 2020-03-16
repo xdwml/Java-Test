ee4e42c056e20af533f42645d7849f75ebe95a74
wml <18270856781@163.com>
2020-03-16 22:14:05 +0800

    Signed-off-by: wml <18270856781@163.com>

---
+++ niuke/src/Stack/Problem21Test.java
@@ -0,0 +1,17 @@
+package Stack;
+
+public class Problem21Test {
+    public class Problem21test {
+        public static void main(String args[])
+        {
+            StackWithMin test=new StackWithMin();
+            test.push(3);
+            test.push(2);
+            test.push(1);
+            //System.out.println(test.pop());
+            System.out.println(test.pop());
+            System.out.println(test.min());
+        }
+
+    }
+}
